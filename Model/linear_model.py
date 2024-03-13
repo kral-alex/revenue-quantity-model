02c3b984dcf1ea8edee2475414544141c74b7c4c
Alex Král <alex.kral.cz@gmail.com>
2024-03-13 17:21:17 +0100

    cleaning fix last_change_slope

--- Model/linear_model.py
+++ Model/linear_model.py
@@ -2,8 +2,6 @@ import numpy as np
 
 from Processing.time_series import PriceQuantity
 
-EPSILON = 1e-9
-
 
 class NotEnoughDataError(ValueError):
     pass
@@ -11,31 +9,77 @@ class NotEnoughDataError(ValueError):
 
 # can be made to take asymmetric mean with weights
 def last_change_slope(pq: PriceQuantity, min_count: int = 1, max_count: int = np.inf) -> float:
-    change_indices = np.argwhere(np.diff(pq.price) > EPSILON).squeeze()
-    d_price = pq.price[-1] - pq.price[change_indices[-1]]
-    len_left = change_indices[-1] - change_indices[-2]
-    len_right = (len(pq) - 1) - change_indices[-1]
+    len_left, middle, len_right = find_change_range(pq)
     if min(len_left, len_right) < min_count:
         raise NotEnoughDataError(
-            f'Not enough data on first price change. Minimum required: {min_count}. Left count: {len_left}. Right count: {len_right}'
+            f'Not enough data on first price change.'
+            f' Minimum required: {min_count}. Left count: {len_left}. Right count: {len_right}'
         )
+
     take_count = min(len_left, len_right, max_count)
-    middle = change_indices[-1]
-    d_quantity = np.mean(pq.quantity[middle : middle + take_count]) - np.mean(pq.quantity[middle - take_count : middle])
+
+    d_quantity = np.mean(
+        pq.quantity[middle: middle + take_count]
+        - pq.quantity[middle - take_count: middle]
+    )
+
+    d_price = pq.price[middle] - pq.price[middle - 1]
+
     return d_quantity / d_price
 
 
-def last_change_with_time_slope(pq, min_count: int = 1, max_count: int = np.inf) -> float:
-    pass  # TODO
+def last_change_with_time_slope(pq, min_count: int = 2, max_count: int = np.inf) -> (float, float):
+    len_left, middle, len_right = find_change_range(pq)
+    if min(len_left, len_right) < min_count:
+        raise NotEnoughDataError(
+            f'Not enough data on first price change. '
+            f'Minimum required: {min_count}. Left count: {len_left}. Right count: {len_right}'
+        )
+
+    take_count = min(len_left, len_right, max_count)
+    half_count = take_count // 2
+    d_quantity_thin = np.mean(
+        pq.quantity[middle: middle + half_count]
+        - pq.quantity[middle - half_count: middle]
+    )
+    d_quantity_wide = np.mean(
+        pq.quantity[middle + half_count: middle + take_count]
+        - pq.quantity[middle - take_count: middle - half_count]
+    )
+
+    d_quantity_t = 0.5 * d_quantity_wide - d_quantity_thin
+    d_quantity_p = 2 * d_quantity_thin - 0.5 * d_quantity_wide
+
+    d_price = pq.price[middle] - pq.price[middle - 1]
+
+    return d_quantity_t / (2 * take_count), d_quantity_p / d_price
 
 
-def find_change_range(pq: PriceQuantity, n: int = 0):
-    n += 1
-    change_indices = np.argwhere(np.diff(pq.price) > EPSILON).squeeze()
-    len_left = change_indices[-n] - change_indices[-(1 + n)]
-    len_right = (len(pq) - n) - change_indices[-n]
-    middle = change_indices[-n]
-    return len_left, middle, len_right
+EPSILON = 1e-9
+
+
+def find_change_range(pq: PriceQuantity, n: int = -1) -> (float, float, float):
+    change_indices = np.argwhere(abs(np.diff(pq.price)) > EPSILON).squeeze(axis=1)
+    if not len(change_indices):
+        raise ValueError('No value changes found')
+
+    if n < 0:
+        n = len(change_indices) + n
+
+    if len(change_indices) <= n:
+        raise ValueError('Index out of range for value changes')
+
+    edge_indices = np.concatenate(([0], change_indices + 1, [len(pq) - 1]))
+
+    i_left = edge_indices[n]
+
+    i_middle = edge_indices[n + 1]
+
+    i_right = edge_indices[n + 2]
+
+    return (i_middle - i_left,
+            i_middle,
+            i_right - i_middle)
 
 
 def linear_model_slope(pq: PriceQuantity, max_count: int = None) -> float:
